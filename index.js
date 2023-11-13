ae5767ab37af49736fc51298a8a542f8e18725d3
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-13 17:59:21 -0300

    boilerPlate Back

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hola");
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const db = require("./db");
+const envs = require("./config/envs");
+
+const authAPI = require("./routes");
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", authAPI);
+
+db.sync({ force: false }).then(() => {
+  console.log("Db connected");
+  app.listen(envs.PORT, () => {
+    console.log(`Server listening at port ${envs.PORT}`);
+  });
+});
