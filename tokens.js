e28f222534214da5dd9f04ccff3b76230be67396
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-13 14:34:57 -0300

    jwt

---
+++ tokens.js
@@ -0,0 +1,13 @@
+const jwt = require("jsonwebtoken");
+
+const { SECRET } = require("escribimos de donde requerimos SECRET");
+
+const generateToken = (payload) => {
+  const token = jwt.sign({ user: payload }, SECRET, { expiresIn: "2d" });
+  return token;
+};
+
+//aqui en hacemos la persistencia del token
+const validateToken = () => {};
+
+module.exports = { generateToken, validateToken };
