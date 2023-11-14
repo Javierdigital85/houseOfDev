74f2340f3c1243b60c53a86b756731c9e419f542
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-14 16:11:42 -0300

    update servidor cors

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const app = express();
 // const cookieParser = require("cookie-parser");
 const db = require("./db");
 const envs = require("./config/envs");
-
+const cors = require("cors");
 const authAPI = require("./routes");
 
 app.use(express.json());
 // app.use(cookieParser());
 
 app.use("/api", authAPI);
-
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 db.sync({ force: false }).then(() => {
   console.log("Db connected");
   app.listen(envs.PORT, () => {
