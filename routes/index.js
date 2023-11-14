5443e1bf966ce598e09a756fd9aa11f9746b72f9
Lorenzo Javier Colodro <javi@Lorenzos-MacBook-Pro.local>
2023-11-14 09:52:46 -0300

    update estructura de carpeta

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userRouter = require("./user");
+const userRouter = require("./users");
 
 router.use("/users", userRouter);
 
