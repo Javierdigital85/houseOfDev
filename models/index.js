678aeebf8d1a5a4d8194f21b9274aab441f349c1
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2023-12-01 15:01:04 -0300

    add favs routes

--- models/index.js
+++ models/index.js
@@ -1,8 +1,11 @@
 const User = require("./User");
 const Property = require("./Property");
 const Visit = require("./Visit");
+const Favs = require("./Favs");
 
 Visit.belongsTo(User, { as: "prospect" });
 Visit.belongsTo(Property, { as: "property" });
+Favs.belongsTo(User, { as: "prospect" });
+Favs.belongsTo(Property, { as: "property" });
 
 module.exports = { User, Property, Visit };
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+
 const userRouter = require("./users");
 const userProperties = require("./properties");
 const userCitas = require("./visits");
+const userFavs = require("./favs");
 
 router.use("/users", userRouter);
 router.use("/properties", userProperties);
 router.use("/visits", userCitas);
+router.use("/favs", userFavs);
 
 module.exports = router;
