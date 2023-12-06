c2060d2358f0839df2360eeba2dec848f662c12f
Hernan Matias Ortiz <hernanortiz18@gmail.com>
2023-12-06 13:12:41 -0300

    add fav route

--- routes/favs.js
+++ routes/favs.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const favsRouter = express.Router();
 const Favs = require("../models/Favs");
-
-favsRouter.get("/", (req, res) => {
-  Favs.findAll().then((favs) => res.status(200).send(favs));
-});
+const Property = require("../models/Property");
+const { promises } = require("nodemailer/lib/xoauth2");
 
 favsRouter.post("/register", (req, res) => {
   const { prospectId, propertyId } = req.body;
@@ -17,6 +15,23 @@ favsRouter.post("/register", (req, res) => {
       res.status(200).send(created);
     })
     .catch((err) => console.error(err));
+});
+favsRouter.get("/", (req, res) => {
+  Favs.findAll({ where: req.query })
+    .then((res) => {
+      const arrayId = res.map((propId) => {
+        return propId.propertyId;
+      });
+      const promesas = arrayId.map((id) => {
+        return Property.findByPk(id);
+      });
+      console.log(promesas);
+      return promesas;
+    })
+    .then((promesas) => {
+      return Promise.all(promesas);
+    })
+    .then((result) => res.status(200).send(result));
 });
 
 module.exports = favsRouter;
